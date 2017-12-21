935db653f2a7019c9509534beb5b060c2363f987
MindaRynn <mindaryn@gmail.com>
2017-12-21 23:16:04 +0700

    create list component and route create new list

---
+++ app/routes/main/components/list.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <li v-for="listName in list">
+          {{ listName.name }}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ['list']
+}
+</script>
+
+<style lang="css">
+
+</style>
--- app/routes/main/main.vue
+++ app/routes/main/main.vue
@@ -1,20 +1,25 @@
 <template>
     <div>
-        <h1>{{ title }}</h1>
         <h2>Events</h2>
-        <ul>
-            <li v-for="event in events">
-                {{ event.name }}
-            </li>
-        </ul>
+        <form method="post">
+          <input type="hidden" name="_csrf" :value="csrfToken">
+          Name : <input type="text" name="name"><br>
+          Details : <textarea name="description" placeholder="add multiple lines"></textarea><br>
+          <button type="submit" value="Submit">Create</button>
+        </form>
+        <list :list="events"></list>
     </div>
 </template>
 
 <script>
+import list from './main/components/list.vue';
 export default {
     data: function () {
         return {
         }
+    },
+    components: {
+      list: list
     }
 }
 </script>
