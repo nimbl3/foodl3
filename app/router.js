4ed3549505e2da2a22d9810ef8d985c23a35169d
Rossukhon Leagmongkol <rose.roko@gmail.com>
2018-02-16 17:01:40 +0700

    Apply event detail page

--- app/router.js
+++ app/router.js
@@ -52,7 +52,7 @@ module.exports.init = (app, config) => {
           { charset: 'UTF-8'},
           { name: 'viewport', content: 'width=device-width, user-scalable=no, initial-scale=1' },
           { script: vueScript },
-          { style: 'assets/rendered/style.css' }
+          { style: '/assets/rendered/style.css' }
         ],
         title: 'Foodl3'
       }
