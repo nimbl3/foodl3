76d17385f0f068675962b7b4eb13d7e659b99e52
Rossukhon Leagmongkol <rose.roko@gmail.com>
2018-02-16 15:23:08 +0700

    Apply event page styling

--- app/components/EventList.vue
+++ app/components/EventList.vue
@@ -1,38 +1,25 @@
 <template>
-  <!--<ul>-->
-    <!--<li v-for="event in events">-->
-      <!--<a :href="`/detail/${event.id}`">-->
-        <!--{{ event.name }}-->
-      <!--</a>-->
-    <!--</li>-->
-  <!--</ul>-->
-
   <table class="event-list">
     <thead>
     <tr class="event-list__divider">
       <th class="event-list__header">Event Name</th>
       <th class="event-list__header">Order Time</th>
       <th class="event-list__header">Member</th>
-      <th class="event-list__header"></th>
+      <th class="event-list__header hidden"></th>
     </tr>
     </thead>
     <tbody>
-      <tr v-for="event in events">
-        <!--<a :href="`/detail/${event.id}`">-->
-          <!--{{ event.name }}-->
-        <!--</a>-->
-
+      <tr v-for="event in events" class="event-list__divider">
         <td>
-          <a class="event-list__order" href="/">{{ event.name }}</a>
+          <a class="event-list__order" :href="`/detail/${event.id}`">{{ event.name }}</a>
         </td>
         <td>
           <span class="event-list__date">{{ event.endDate }}</span>
         </td>
         <td>
-          <ul class="user-list">
-          </ul>
+          <user-list :users=event.users></user-list>
         </td>
-        <td>
+        <td class="hidden">
           <a href="/" class="event-list__action">
             <i class="icon icon--edit"></i>
             <span>Edit</span>
@@ -48,7 +35,15 @@
 </template>
 
 <script>
+  import userList from './UserList.vue';
+
   export default {
-    props: ['events']
+    props: ['events'],
+    components: {
+      userList
+    },
+    created() {
+      console.log(this.events);
+    }
   };
 </script>
