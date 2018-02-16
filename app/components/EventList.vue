87496aaa73079e609e26fc83279ac62485dc6d0b
Rossukhon Leagmongkol <rose.roko@gmail.com>
2018-02-16 14:21:02 +0700

    Apply event page styling

--- app/components/EventList.vue
+++ app/components/EventList.vue
@@ -1,11 +1,50 @@
 <template>
-  <ul>
-    <li v-for="event in events">
-      <a :href="`/detail/${event.id}`">
-        {{ event.name }}
-      </a>
-    </li>
-  </ul>
+  <!--<ul>-->
+    <!--<li v-for="event in events">-->
+      <!--<a :href="`/detail/${event.id}`">-->
+        <!--{{ event.name }}-->
+      <!--</a>-->
+    <!--</li>-->
+  <!--</ul>-->
+
+  <table class="event-list">
+    <thead>
+    <tr class="event-list__divider">
+      <th class="event-list__header">Event Name</th>
+      <th class="event-list__header">Order Time</th>
+      <th class="event-list__header">Member</th>
+      <th class="event-list__header"></th>
+    </tr>
+    </thead>
+    <tbody>
+      <tr v-for="event in events">
+        <!--<a :href="`/detail/${event.id}`">-->
+          <!--{{ event.name }}-->
+        <!--</a>-->
+
+        <td>
+          <a class="event-list__order" href="/">{{ event.name }}</a>
+        </td>
+        <td>
+          <span class="event-list__date">{{ event.endDate }}</span>
+        </td>
+        <td>
+          <ul class="user-list">
+          </ul>
+        </td>
+        <td>
+          <a href="/" class="event-list__action">
+            <i class="icon icon--edit"></i>
+            <span>Edit</span>
+          </a>
+          <a href="/" class="event-list__action">
+            <i class="icon icon--delete"></i>
+            <span>Delete</span>
+          </a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
--- app/screens/Events.vue
+++ app/screens/Events.vue
@@ -1,24 +1,32 @@
 <template>
   <div class="screen-event">
     <app-header :current-user="currentUser"></app-header>
-    <h2>Events</h2>
-    <form action="/event" method="post">
-      <input type="hidden" name="_csrf" :value="csrfToken">
-      <div>
-        <label>Name</label>
-        <input type="text" name="name" placeholder="Event Name" required>
-      </div>
-      <div>
-        <label>Order time</label>
-        <input type="datetime-local" name="end_date" required>
-      </div>
-      <div>
-        <label>Details</label>
-        <textarea name="description" placeholder="Event Details"></textarea>
-      </div>
-      <button type="submit">Add</button>
-    </form>
-    <event-list :events="events"></event-list>
+    <main>
+      <form style="display: none" action="/event" method="post">
+        <input type="hidden" name="_csrf" :value="csrfToken">
+        <div>
+          <label>Name</label>
+          <input type="text" name="name" placeholder="Event Name" required>
+        </div>
+        <div>
+          <label>Order time</label>
+          <input type="datetime-local" name="end_date" required>
+        </div>
+        <div>
+          <label>Details</label>
+          <textarea name="description" placeholder="Event Details"></textarea>
+        </div>
+        <button type="submit">Add</button>
+      </form>
+
+      <section class="event">
+        <header class="event__header">
+          <h1 class="event__header-title">Event</h1>
+          <a href="/" class="btn btn--primary btn--large">&plus; Add event</a>
+        </header>
+        <event-list :events="events"></event-list>
+      </section>
+    </main>
   </div>
 </template>
 
