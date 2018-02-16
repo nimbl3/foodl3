2e4b7c3f946e234201c1d3607bf0bcd699443d24
Rossukhon Leagmongkol <rose.roko@gmail.com>
2018-02-16 11:54:50 +0700

    Apply login page style

--- app/screens/Login.vue
+++ app/screens/Login.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="screen login">
-    <h1>Foodl3</h1>
-    <a v-bind:href="loginUrl">
-      <img alt="Sign in with Slack" height="40" width="172"
-           src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
-           srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"/>
-    </a>
+    <section class="authentication">
+      <h1 class="authentication__logo">Foodl3</h1>
+      <p class="authentication__taglines">Let’s eat together!</p>
+
+      <a v-bind:href="loginUrl">
+        <img class="authentication__with-slack"
+             src="/assets/images/login@1x.png"
+             srcset="/assets/images/login@2x.png"
+             alt="Login with Slack">
+      </a>
+    </section>
   </div>
 </template>
 
