cb795db783bb4321c7225d1de1d697bc554732fd
Rossukhon Leagmongkol <rose.roko@gmail.com>
2018-03-15 14:10:21 +0700

    Remove commented code

--- app/components/OrderList.vue
+++ app/components/OrderList.vue
@@ -35,27 +35,6 @@
     </tr>
     </tbody>
   </table>
-
-  <!--<ul>-->
-    <!--<li v-for="order in orders">-->
-      <!--<div class="order-container">-->
-        <!--<dl>-->
-          <!--<dt>Name</dt>-->
-          <!--<dd>{{order.name}}</dd>-->
-          <!--<dt>Price</dt>-->
-          <!--<dd>{{order.price}}</dd>-->
-          <!--<dt>Link</dt>-->
-          <!--<dd>{{order.link}}</dd>-->
-          <!--<user-list :users="order.users"></user-list>-->
-          <!--<form :action="`/order/${order.id}/join`" method="post">-->
-            <!--<input type="hidden" name="_csrf" :value="token">-->
-            <!--<input type="hidden" name="event_id" :value="event.id">-->
-            <!--<button type="submit">Join</button>-->
-          <!--</form>-->
-        <!--</dl>-->
-      <!--</div>-->
-    <!--</li>-->
-  <!--</ul>-->
 </template>
 
 <script>
